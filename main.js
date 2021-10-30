444aadb6fc87824961ffdac042ac33fdbe73b332
lucas martinez <patrasioo69@gmail.com>
2021-10-30 21:18:14 +0200

    Functions display added

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-//Funcion ocultar HomeScreen
-function ocultarHomeScreen(){
+//Funcion display markerScreen
+function displayMarkerScreen(){
     let divHome = document.getElementById('homeScreen');
     divHome.style.display = 'none';
-    let divMarker = document.getElementById('markerScreen')
+    let divMarker = document.getElementById('markerScreen');
     divMarker.style.display = 'block';
 }
-document.getElementById('start').addEventListener('click', ocultarHomeScreen);
+document.getElementById('start').addEventListener('click', displayMarkerScreen);
+
+//funcion return homeScreen
+function returnHomeScreen(){
+    let divHome = document.getElementById('homeScreen');
+    divHome.style.display = 'block';
+    let divMarker = document.getElementById('markerScreen');
+    divMarker.style.display = 'none';
+}
+document.getElementById('return').addEventListener('click', returnHomeScreen);
